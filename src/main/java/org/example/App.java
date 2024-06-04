f16c5720cb2e1d641e1ca9a199e899f72188c64c
Pepi <petranka_popova_@abv.bg>
2024-06-04 16:46:56 +0300

    Initial commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,7 +5,7 @@ package org.example;
  */
 public class App {
     public static void main(String[] args) {
-        MultiMap multiMap = new MultiMap();
+        MultiMap<String, Integer> multiMap = new MultiMap<>();
         multiMap.put("Pepi", 6);
         System.out.println(multiMap.get("Pepi"));
 
